c31e0369ec58cd5a9f492d4b0d3b576e4a74978b
Pedro Arenas <pedroars99@icloud.com>
2023-08-31 23:18:26 +0200

    Set up icon library

---
+++ ui/src/lib/components/Icon.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Fa from 'svelte-fa/src/fa.svelte';
+	import { faDumbbell, faHeart, type IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
+	export let icon: Icon;
+
+	type Icon = 'dumbbell' | 'heart';
+
+	const icons: { [key: string]: IconDefinition } = {
+		dumbbell: faDumbbell,
+		heart: faHeart
+	};
+</script>
+
+<Fa icon={icons[icon]} />
