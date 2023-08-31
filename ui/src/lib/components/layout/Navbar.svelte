492a4d6e5e07e2d95d5d72320f9457e9e2e39068
Pedro Arenas <pedroars99@icloud.com>
2023-08-31 23:20:04 +0200

    Add navbar

---
+++ ui/src/lib/components/layout/Navbar.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Icon from '$lib/components/Icon.svelte';
+</script>
+
+<div class="navbar bg-base-100">
+	<div class="navbar-start">
+	</div>
+	<div class="navbar-center">
+		<a href="/" class="btn btn-ghost normal-case text-xl">
+        <Icon icon='heart' />
+        <Icon icon='dumbbell' />
+		</a>
+	</div>
+	<div class="navbar-end">
+	</div>
+</div>
