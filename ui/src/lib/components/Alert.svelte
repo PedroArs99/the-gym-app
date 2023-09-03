822a20bbe75af8636bc8a9625702ce6b2882766c
Pedro Arenas <pedroars99@icloud.com>
2023-09-03 19:26:31 +0200

    Add Alert component

---
+++ ui/src/lib/components/Alert.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Icon from "./Icon.svelte";
+
+  export let message:string; 
+  export let severity: 'error' | 'info' | 'success' | 'warning';
+
+  $: severityClass = `alert alert-${severity}`
+</script>
+
+
+
+<div class={severityClass}>
+  <Icon icon={severity} />
+  <span>{ message }</span>
+</div>
