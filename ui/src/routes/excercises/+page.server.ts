f35004ed524f31133add978f8fa7c241273d2e95
Pedro Arenas <pedroars99@icloud.com>
2023-09-03 20:11:48 +0200

    Fetch Dynamo Excercises

--- ui/src/routes/excercises/+page.server.ts
+++ ui/src/routes/excercises/+page.server.ts
@@ -1,14 +1,15 @@
 import type { Excercise } from '$lib/models/excercise.model';
 import { Muscles } from '$lib/models/muscles.enum';
+import axios from 'axios';
 
-export function load() {
+const apiUrl = 'https://5rjvidrtda.execute-api.eu-central-1.amazonaws.com';
+
+export async function load() {
+	const response = await axios.get(`${apiUrl}/excercises`);
+	const excercises = response.data;
+
 	return {
-		excercises: [
-			{
-				id: '2027ac88-2d18-4889-8ff6-92a753a56001',
-				name: 'Squat',
-				muscle: Muscles.LEGS
-			}
-		]
-	};
+		excercises,
+	}
 }
+
