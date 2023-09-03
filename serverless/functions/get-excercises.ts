790bfd22e65a3ac5a7227325cf849ab918d3cd64
Pedro Arenas <pedroars99@icloud.com>
2023-09-03 20:10:42 +0200

    Add Get excercises function

---
+++ serverless/functions/get-excercises.ts
@@ -0,0 +1,38 @@
+import { HttpRequest } from "../models/http-request";
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { Excercise } from "../models/excercise";
+
+const EXCERCISES_TABLE = process.env.EXCERCISES_TABLE;
+const client = new DynamoDBClient();
+const dynamoDbClient = DynamoDBDocumentClient.from(client);
+
+module.exports.handler = async (event: HttpRequest) => {
+  try {
+    const queryResult = await dynamoDbClient.send(
+      new ScanCommand({
+        TableName: EXCERCISES_TABLE,
+        ConsistentRead: true,
+      })
+    );
+
+    const items = queryResult.Items ?? [];
+
+    const response = items?.map((item) => ({
+      id: item.id.S,
+      name: item.name.S,
+      muscle: item.muscle.S,
+    }));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: error,
+    };
+  }
+};
