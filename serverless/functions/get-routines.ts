d70dfecced7dd30e5f0dd2d77ca5ff8193816b81
Pedro Arenas <pedroars99@icloud.com>
2023-09-16 19:51:39 +0200

    Sort routines before mapping

--- serverless/functions/get-routines.ts
+++ serverless/functions/get-routines.ts
@@ -4,12 +4,9 @@ import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import { Routine } from "../models/routine.model";
 import { dateLocale, dateFormatOptions } from "../config/date-format.config";
 
-
-
 const ROUTINES_TABLE = process.env.ROUTINES_TABLE;
 const client = new DynamoDBClient();
 const dynamoDbClient = DynamoDBDocumentClient.from(client);
-
 
 module.exports.handler = async (event: HttpRequest) => {
   try {
@@ -23,15 +20,18 @@ module.exports.handler = async (event: HttpRequest) => {
     const items = queryResult.Items ?? [];
 
     const response: Routine[] = items
+      .sort(
+        (a, b) =>
+          new Date(a.createdAt.S!).getTime() - new Date(b.createdAt.S!).getTime()
+      )
       .map((item) => ({
         id: item.id.S!,
         name: item.name.S!,
-        createdAt: new Date(item.createdAt.S!).toLocaleDateString(dateLocale, dateFormatOptions),
-      }))
-      .sort(
-        (a, b) =>
-          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
-      );
+        createdAt: new Date(item.createdAt.S!).toLocaleDateString(
+          dateLocale,
+          dateFormatOptions
+        ),
+      }));
 
     return {
       statusCode: 200,
