8c571e8d0b9201968924da7d6f00c775553f259f
Pedro Arenas <pedroars99@icloud.com>
2023-09-18 22:36:30 +0200

    Add Workout Component

---
+++ ui/src/lib/components/routines/Workout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import type { Workout } from "$lib/models/routine.model";
+
+  export let workout: Workout;
+</script>
+
+<h2 class="page-subtitle">Workout #{workout.number}</h2>
