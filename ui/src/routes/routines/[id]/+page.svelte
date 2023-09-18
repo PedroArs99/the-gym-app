f54368c8c7acfefc6b11886b98d865f59c3dc25f
Pedro Arenas <pedroars99@icloud.com>
2023-09-18 22:36:38 +0200

    Display workouts

--- ui/src/routes/routines/[id]/+page.svelte
+++ ui/src/routes/routines/[id]/+page.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
-  export let data;
+	import Icon from '$lib/components/Icon.svelte';
+	import Workout from '$lib/components/routines/Workout.svelte';
+	import type { Routine, Workout as WorkoutModel } from '$lib/models/routine.model';
+
+	export let data: {
+		routine: Routine;
+	};
+
+	let workouts = data.routine.workouts ?? [];
+
+	function newWorkout() {
+		const nextNumber = workouts.length + 1;
+		const newWorkout: WorkoutModel = {
+			number: nextNumber,
+			excercises: []
+		};
+
+		workouts = [...workouts, newWorkout];
+	}
 </script>
 
 <svelte:head>
@@ -8,3 +26,11 @@
 </svelte:head>
 
 <h1 class="page-title">{data.routine.name}</h1>
+
+{#each workouts as workout}
+	<Workout {workout} />
+{/each}
+
+<button on:click={newWorkout} class="btn sticky bottom-0">
+	<Icon icon="plus" />
+</button>
