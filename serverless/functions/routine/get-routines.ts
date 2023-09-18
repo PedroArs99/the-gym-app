614367b2d035eff40db908055293194bbb1f1335
Pedro Arenas <pedroars99@icloud.com>
2023-09-18 23:56:39 +0200

    refactor get-routines function

--- serverless/functions/routine/get-routines.ts
+++ serverless/functions/routine/get-routines.ts
@@ -1,8 +1,7 @@
 import { HttpRequest } from "../../models/http-request.model";
 import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
-import { Routine } from "../../models/routine.model";
-import { dateLocale, dateFormatOptions } from "../../config/date-format.config";
+import { Routine, mapDbEntityToModel } from "../../models/routine.model";
 
 const ROUTINES_TABLE = process.env.ROUTINES_TABLE;
 const client = new DynamoDBClient();
@@ -24,14 +23,7 @@ module.exports.handler = async (event: HttpRequest) => {
         (a, b) =>
           new Date(a.createdAt.S!).getTime() - new Date(b.createdAt.S!).getTime()
       )
-      .map((item) => ({
-        id: item.id.S!,
-        name: item.name.S!,
-        createdAt: new Date(item.createdAt.S!).toLocaleDateString(
-          dateLocale,
-          dateFormatOptions
-        ),
-      }));
+      .map((item) => mapDbEntityToModel(item));
 
     return {
       statusCode: 200,
