c081bc596c33c389044422e3676eb250268aafdd
Pedro Arenas <pedroars99@icloud.com>
2023-09-18 23:57:46 +0200

    Save workouts on dynamo

--- serverless/models/routine.model.ts
+++ serverless/models/routine.model.ts
@@ -1,20 +1,44 @@
 import { AttributeValue } from "@aws-sdk/client-dynamodb";
 import { dateLocale, dateFormatOptions } from "../config/date-format.config";
+import { Excercise } from "./excercise.model";
 
 export interface Routine {
   id: string;
   /* As ISO String */
   createdAt: String;
   name: string;
+  workouts: Workout[];
 }
 
-export function mapDbEntityToModel(item: Record<string, AttributeValue>): Routine {
+export function isRoutineValid(routine: Routine): boolean {
+  return !!routine.name;
+}
+
+export interface Workout {
+  number: number;
+  excercises: Excercise[];
+}
+
+export function mapDbEntityToModel(
+  item: Record<string, AttributeValue>
+): Routine {
   return {
     id: item.id.S!,
-    name: item.name.S!,
     createdAt: new Date(item.createdAt.S!).toLocaleDateString(
       dateLocale,
       dateFormatOptions
     ),
+    name: item.name.S!,
+    workouts: flattenWorkouts(item.workouts.L ?? []),
   };
+}
+
+function flattenWorkouts(workouts: AttributeValue[]): Workout[] {
+  return workouts
+    .filter(workout => !!workout.M)
+    .map((workout) => workout.M!)
+    .map((workout) => ({
+      number: +workout.number.N!,
+      excercises: new Array<Excercise>,
+    }));
 }
