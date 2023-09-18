d26668950dd2b86195eefdda5659fa754eab670f
Pedro Arenas <pedroars99@icloud.com>
2023-09-18 22:36:21 +0200

    define workout model

--- ui/src/lib/models/routine.model.ts
+++ ui/src/lib/models/routine.model.ts
@@ -1,5 +1,13 @@
+import type { Excercise } from "./excercise.model";
+
 export interface Routine {
   id: string;
   createdAt: Date;
   name: string;
+  workouts: Workout[];
+}
+
+export interface Workout {
+  number: number;
+  excercises: Excercise[];
 }
