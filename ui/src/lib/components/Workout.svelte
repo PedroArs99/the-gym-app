1f0fa6f8be9262a62ede4342e928f4b300046a4a
Pedro Arenas <pedroars99@icloud.com>
2023-09-18 23:58:02 +0200

    Move Workout card

---
+++ ui/src/lib/components/Workout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Workout } from "$lib/models/routine.model";
+
+  export let workout: Workout;
+</script>
+
+<div class="card">
+  <div class="card-body">
+    <div class="card-title">Workout #{workout.number}</div>
+  </div>
+</div>
+
+<style lang="postcss">
+  .card {
+    @apply border;
+  }
+</style>
--- ui/src/lib/components/routines/Workout.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import type { Workout } from "$lib/models/routine.model";
-
-  export let workout: Workout;
-</script>
-
-<h2 class="page-subtitle">Workout #{workout.number}</h2>
