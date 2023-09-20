2a6cff09c272a287eca3f74ff4f755ca5e7b9bdb
Pedro Arenas <pedroars99@icloud.com>
2023-09-20 22:44:15 +0200

    Add new Excercise icon

--- ui/src/lib/components/Workout.svelte
+++ ui/src/lib/components/Workout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { Workout } from "$lib/models/routine.model";
+	import Icon from "./Icon.svelte";
 
   export let workout: Workout;
 </script>
@@ -7,11 +8,28 @@
 <div class="card">
   <div class="card-body">
     <div class="card-title">Workout #{workout.number}</div>
+    <div class="excercises">
+      <button class="new-excercise-button">
+        <Icon icon="plus" size="lg"/>
+      </button>
+    </div>
   </div>
 </div>
 
 <style lang="postcss">
   .card {
-    @apply border;
+    @apply border border-primary rounded-2xl;
+  }
+
+  .card-title {
+    @apply text-2xl;
+  }
+
+  .new-excercise-button {
+    @apply border-4 border-dashed p-8;
+  }
+
+  .new-excercise-button:hover {
+    @apply bg-base-200;
   }
 </style>
