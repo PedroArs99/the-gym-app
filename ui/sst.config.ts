23262c6418a796033cd88c64071d5aa06b3564fd
Pedro Arenas <pedroars99@icloud.com>
2023-11-02 18:08:30 +0100

    Fix SST Deployment

--- ui/sst.config.ts
+++ ui/sst.config.ts
@@ -1,19 +1,28 @@
-import type { SSTConfig } from "sst";
-import { SvelteKitSite } from "sst/constructs";
+import type { SSTConfig } from 'sst';
+import { Config, SvelteKitSite } from 'sst/constructs';
 
 export default {
-  config(_input) {
-    return {
-      name: "the-gym-app",
-      region: "eu-central-1",
-    };
-  },
-  stacks(app) {
-    app.stack(function Site({ stack }) {
-      const site = new SvelteKitSite(stack, "site");
-      stack.addOutputs({
-        url: site.url,
-      });
-    });
-  },
+	config(_input) {
+		return {
+			name: 'the-gym-app',
+			region: 'eu-central-1'
+		};
+	},
+	stacks(app) {
+		app.stack(function Site({ stack }) {
+			const API_URL = new Config.Parameter(stack, 'API_URL', {
+				value: 'https://5rjvidrtda.execute-api.eu-central-1.amazonaws.com'
+			});
+
+			const site = new SvelteKitSite(stack, 'site', {
+				customDomain: 'the-fit-app.pro',
+				environment: {
+					API_URL: API_URL.value
+				}
+			});
+			stack.addOutputs({
+				url: site.url
+			});
+		});
+	}
 } satisfies SSTConfig;
